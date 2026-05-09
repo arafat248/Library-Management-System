45b2e78d842db410e8f01aead896123341261625
arafat248 <aab.arafat248@gmail.com>
2026-05-09 14:47:31 +0600

    create CategoryViewSet

--- category/views.py
+++ category/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Category
+from .serializer import CategorySreializer
 
-# Create your views here.
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySreializer
